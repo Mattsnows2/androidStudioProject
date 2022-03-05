22a8a7b2acf65c84b698a8f1060408e02c0e3b7b
Matthias BELOT <matthias.belot@isep.fr>
2022-03-05 10:41:54 +0100

    feat : Implementation intent to launch loginPage

--- app/src/main/java/com/example/project/HomeActivity.java
+++ app/src/main/java/com/example/project/HomeActivity.java
@@ -2,13 +2,29 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public String Tag="inforamtion";
+    Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        buttonLogin=findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
