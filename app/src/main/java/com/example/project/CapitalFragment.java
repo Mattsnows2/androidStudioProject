d3ec45e93ce489c5416f3c2bfb19fb40aa6da89a
matus <matuscactus@gmail.com>
2022-05-13 20:13:25 +0200

    dashboard

--- app/src/main/java/com/example/project/CapitalFragment.java
+++ app/src/main/java/com/example/project/CapitalFragment.java
@@ -17,30 +17,27 @@ import com.example.project.placeholder.PlaceholderContent;
 /**
  * A fragment representing a list of Items.
  */
-public class transactionFragment extends Fragment {
+public class CapitalFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
     private int mColumnCount = 4;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
-    public transactionFragment() {
+    public CapitalFragment() {
     }
 
-    // TODO: Customize parameter initialization
     @SuppressWarnings("unused")
-    public static transactionFragment newInstance(int columnCount) {
-        transactionFragment fragment = new transactionFragment();
+    /*public static CapitalFragment newInstance(int columnCount) {
+        CapitalFragment fragment = new CapitalFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
         return fragment;
-    }
-
+    }*/
+/*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,12 +45,15 @@ public class transactionFragment extends Fragment {
         if (getArguments() != null) {
             mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
         }
-    }
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transaction_list, container, false);
+        /* FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameTransactions, new transactionFragment());
+        transaction.commit();*/
 
         // Set the adapter
         if (view instanceof RecyclerView) {
--- app/src/main/java/com/example/project/DashboardActivity.java
+++ app/src/main/java/com/example/project/DashboardActivity.java
@@ -1,14 +1,35 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    Button capital_btn;
+    Button receipts_btn;
+    Button expenses_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        capital_btn = findViewById(R.id.capitalBtn);
+        receipts_btn = findViewById(R.id.receiptsBtn);
+        expenses_btn = findViewById(R.id.expensesBtn);
+
+        NavController navController = Navigation.findNavController(this, R.id.capitalFragment);
+
+        capital_btn.setOnClickListener(v-> navController.navigate(R.id.capitalFragment));
+        /*receipts_btn.setOnClickListener(v-> navController.navigate(R.id.capitalFragment));
+        expenses_btn.setOnClickListener(v-> navController.navigate(R.id.capitalFragment));*/
+
+
     }
 }
+
+
