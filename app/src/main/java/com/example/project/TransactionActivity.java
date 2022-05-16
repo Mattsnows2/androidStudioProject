70f5391599fb270b29fbcd37b68c7dc0ab757def
Mattsnows2 <matthias.belot54@gmail.com>
2022-05-16 15:34:39 +0200

    exepsnes et receipts

--- app/src/main/java/com/example/project/TransactionActivity.java
+++ app/src/main/java/com/example/project/TransactionActivity.java
@@ -1,7 +1,9 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,11 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import Models.Expenses;
 import Models.Receipts;
 
 public class TransactionActivity extends AppCompatActivity {
@@ -47,13 +53,49 @@ public class TransactionActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String[] capital3 = new String[1];
                 Log.d("tdst", typeOperation.getText().toString());
                 if(typeOperation.getText().toString().equals(s4)){
                     Receipts receipts = new Receipts("lebel","car",Double.parseDouble(operation.getText().toString()));
                     mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("receipts").child("operation").setValue(Double.parseDouble(operation.getText().toString()));
-                    mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("receipts").child("categories").setValue((categories.getText().toString()));
-                }else{
-                    Log.d("pas recipts","pas receipts");
+                    mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("receipts").child("operation").child("categorie").setValue((categories.getText().toString()));
+                    Task<DataSnapshot> capital2 =  mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("capital").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DataSnapshot> task) {
+                            if (!task.isSuccessful()) {
+                                Log.e("firebase", "Error getting data", task.getException());
+                            }
+                            else {
+                                Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                                capital3[0] = String.valueOf(task.getResult().getValue());
+                                mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("capital").setValue((Double.parseDouble(capital3[0])+Double.parseDouble(operation.getText().toString())));
+                                Intent intentStep2=new Intent(TransactionActivity.this, DashboardActivity.class);
+                                startActivity(intentStep2);
+
+                            }
+                        }
+                    });
+                    }else{
+                    Expenses expenses = new Expenses("label","categories", Double.parseDouble((operation.getText().toString())));
+                    mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("expenses").child("operation").setValue(Double.parseDouble(operation.getText().toString()));
+                    mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("expenses").child("categories").setValue((categories.getText().toString()));
+                    Task<DataSnapshot> capital2 =  mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("capital").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DataSnapshot> task) {
+                            if (!task.isSuccessful()) {
+                                Log.e("firebase", "Error getting data", task.getException());
+                            }
+                            else {
+                                Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                                capital3[0] = String.valueOf(task.getResult().getValue());
+                                mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("capital").setValue((Double.parseDouble(capital3[0])-Double.parseDouble(operation.getText().toString())));
+                                Intent intentStep2=new Intent(TransactionActivity.this, DashboardActivity.class);
+                                startActivity(intentStep2);
+
+                            }
+                        }
+                    });
+
                 }
             }
         });
